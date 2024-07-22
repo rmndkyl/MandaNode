902283f33616741bd764f518cb88ce402d3ccbed
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-07-22 11:48:35 +0700

    Update logo.sh

--- WM/logo.sh
+++ WM/logo.sh
@@ -5,10 +5,11 @@ echo -e "\e[1;36m
 ██║░░░░░███████║░╚████╔╝░█████╗░░██████╔╝  ███████║██║██████╔╝██║░░██║██████╔╝██║░░██║██████╔╝
 ██║░░░░░██╔══██║░░╚██╔╝░░██╔══╝░░██╔══██╗  ██╔══██║██║██╔══██╗██║░░██║██╔══██╗██║░░██║██╔═══╝░
 ███████╗██║░░██║░░░██║░░░███████╗██║░░██║  ██║░░██║██║██║░░██║██████╔╝██║░░██║╚█████╔╝██║░░░░░
-╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝  ╚═╝░░╚═╝╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░░░░";
-echo -e "The script and tutorial were written by Telegram user \033[48;2;9;10;12m@rmndkyl\e[0m, \e[1;36mfree and open source, please do not believe in the paid version";
-echo -e "\e[1;36mNode community Telegram channel: \033[48;2;9;10;12mhttps://t.me/layerairdrop\e[0m";
-echo -e "\e[1;36mNode community Telegram group: \033[48;2;9;10;12mhttps://t.me/layerairdropdiskusi\e[0m";
+╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝  ╚═╝░░╚═╝╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░░░░
+";
+echo -e "The script and tutorial were written by Telegram user \e[1;36m@rmndkyl\e[0m, \e[1;36mfree and open source, please do not believe in the paid version";
+echo -e "\e[1;36mNode community Telegram channel: \e[1;36mhttps://t.me/layerairdrop\e[0m";
+echo -e "\e[1;36mNode community Telegram group: \e[1;36mhttps://t.me/layerairdropdiskusi";
 echo -e "\e[0m"
 echo -e ""
 echo -e ""
