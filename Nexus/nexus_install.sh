3aaab76f363b0741cc5080f302da47004df242ed
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-07-22 11:58:53 +0700

    Update nexus_install.sh

--- Nexus/nexus_install.sh
+++ Nexus/nexus_install.sh
@@ -2,7 +2,7 @@
 
 echo "Showing Animation.."
 wget -O loader.sh https://raw.githubusercontent.com/rmndkyl/MandaNode/main/WM/loader.sh && chmod +x loader.sh && sed -i 's/\r$//' loader.sh && ./loader.sh
-curl -s https://raw.githubusercontent.com/rmndkyl/MandaNode/main/WM/logo.sh | bash
+wget -O logo.sh https://raw.githubusercontent.com/rmndkyl/MandaNode/main/WM/logo.sh && chmod +x logo.sh && sed -i 's/\r$//' logo.sh && ./logo.sh
 sleep 2
 
 print_message() {
