c1138caf879a610ac84fca112c1ee44149bfb831
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-08-08 17:17:40 +0700

    Update Artela.sh

--- Artela-Nodes/Artela.sh
+++ Artela-Nodes/Artela.sh
@@ -5,27 +5,29 @@ wget -O loader.sh https://raw.githubusercontent.com/rmndkyl/MandaNode/main/WM/lo
 wget -O logo.sh https://raw.githubusercontent.com/rmndkyl/MandaNode/main/WM/logo.sh && chmod +x logo.sh && sed -i 's/\r$//' logo.sh && ./logo.sh
 sleep 2
 
-# Check if the script is running as root
+#!/bin/bash
+
+# Check if the script is run as root
 if [ "$(id -u)" != "0" ]; then
-    echo "This script needs to be run as root."
-    echo "Please try using 'sudo -i' to switch to the root user, then run this script again."
+    echo "This script must be run as root."
+    echo "Please try switching to the root user using 'sudo -i' and then run this script again."
     exit 1
 fi
 
 # Check and install Node.js and npm
 function install_nodejs_and_npm() {
     if command -v node > /dev/null 2>&1; then
-        echo "Node.js is already installed"
+        echo "Node.js is already installed."
     else
-        echo "Node.js is not installed, installing..."
+        echo "Node.js is not installed, installing now..."
         curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
         sudo apt-get install -y nodejs
     fi
 
     if command -v npm > /dev/null 2>&1; then
-        echo "npm is already installed"
+        echo "npm is already installed."
     else
-        echo "npm is not installed, installing..."
+        echo "npm is not installed, installing now..."
         sudo apt-get install -y npm
     fi
 }
@@ -33,14 +35,14 @@ function install_nodejs_and_npm() {
 # Check and install PM2
 function install_pm2() {
     if command -v pm2 > /dev/null 2>&1; then
-        echo "PM2 is already installed"
+        echo "PM2 is already installed."
     else
-        echo "PM2 is not installed, installing..."
+        echo "PM2 is not installed, installing now..."
         npm install pm2@latest -g
     fi
 }
 
-# Function to automatically set aliases
+# Automatically set up a shortcut alias
 function check_and_set_alias() {
     local alias_name="art"
     local shell_rc="$HOME/.bashrc"
@@ -54,14 +56,14 @@ function check_and_set_alias() {
 
     # Check if the alias is already set
     if ! grep -q "$alias_name" "$shell_rc"; then
-        echo "Setting alias '$alias_name' to $shell_rc"
+        echo "Setting up alias '$alias_name' in $shell_rc"
         echo "alias $alias_name='bash $SCRIPT_PATH'" >> "$shell_rc"
-        # Remind the user to activate the alias
-        echo "Alias '$alias_name' is set. Please run 'source $shell_rc' to activate the alias, or reopen the terminal."
+        # Inform the user to activate the alias
+        echo "Alias '$alias_name' has been set. Please run 'source $shell_rc' to activate the alias, or reopen the terminal."
     else
-        # If the alias is already set, provide a hint
+        # If the alias is already set, provide a reminder
         echo "Alias '$alias_name' is already set in $shell_rc."
-        echo "If the alias does not work, try running 'source $shell_rc' or reopen the terminal."
+        echo "If the alias doesn't work, try running 'source $shell_rc' or reopen the terminal."
     fi
 }
 
@@ -70,7 +72,7 @@ function install_node() {
     install_nodejs_and_npm
     install_pm2
 
-    # Set variable
+    # Set variables
     read -r -p "Please enter the node name you want to set: " NODE_MONIKER
     export NODE_MONIKER=$NODE_MONIKER
 
@@ -89,7 +91,7 @@ function install_node() {
     cd $HOME
     git clone https://github.com/artela-network/artela
     cd artela
-    git checkout v0.4.7-rc7-fix-execution 
+    git checkout v0.4.8-rc8
     make install
     
     cd $HOME
@@ -100,14 +102,13 @@ function install_node() {
     mv $HOME/artelad /usr/local/bin/
     echo 'export LD_LIBRARY_PATH=$HOME/libs:$LD_LIBRARY_PATH' >> ~/.bash_profile
     source ~/.bash_profile
-    
 
     # Configure artelad
     artelad config chain-id artela_11822-1
     artelad init "$NODE_MONIKER" --chain-id artela_11822-1
     artelad config node tcp://localhost:3457
 
-    # Get initial files and address book
+    # Get genesis file and address book
     curl -L https://snapshots.dadunode.com/artela/genesis.json > $HOME/.artelad/config/genesis.json
     curl -L https://snapshots.dadunode.com/artela/addrbook.json > $HOME/.artelad/config/addrbook.json
 
@@ -128,34 +129,19 @@ function install_node() {
     sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:3458\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:3457\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:3460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:3456\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":3466\"%" $HOME/.artelad/config/config.toml
     sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:3417\"%; s%^address = \":8080\"%address = \":3480\"%; s%^address = \"localhost:9090\"%address = \"0.0.0.0:3490\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:3491\"%; s%:8545%:3445%; s%:8546%:3446%; s%:6065%:3465%" $HOME/.artelad/config/app.toml
     echo "export Artela_RPC_PORT=$node_address" >> $HOME/.bash_profile
-    source $HOME/.bash_profile
+    source $HOME/.bash_profile   
 
-    # Reset node
-    artelad tendermint unsafe-reset-all --home $HOME/.artelad
+    pm2 start artelad -- start && pm2 save && pm2 startup
+    
+    # Download snapshot
+    artelad tendermint unsafe-reset-all --home $HOME/.artelad --keep-addr-book
+    curl -L https://snapshots.dadunode.com/artela/artela_latest_tar.lz4 | tar -I lz4 -xf - -C $HOME/.artelad/data
 
-    # Create a service
-    sudo tee /etc/systemd/system/artelad.service > /dev/null <<EOF
-[Unit]
-Description=Artela Node
-After=network-online.target
+    # Start the node process with PM2
+    pm2 restart artelad
 
-[Service]
-User=$USER
-ExecStart=$(which artelad) start
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-    # Register and start the service
-    sudo systemctl daemon-reload
-    sudo systemctl enable artelad
-    sudo systemctl start artelad
-
-    echo "Node setup completed."
+    echo '====================== Installation complete. Please exit the script and run source $HOME/.bash_profile to load environment variables ==========================='
+    
 }
 
 # Check Artela service status
@@ -169,56 +155,6 @@ function view_logs() {
 }
 
 # Uninstall node function
-function uninstall_node() {
-    echo "Are you sure you want to uninstall the Artela node program? This will delete all related data. [Y/N]"
-    read -r -p "Please confirm: " response
-
-    case "$response" in
-        [yY][eE][sS]|[yY]) 
-            echo "Starting the uninstallation of the node program..."
-            pm2 stop artelad && pm2 delete artelad
-            rm -rf $HOME/.artelad $HOME/artela $(which artelad)
-            echo "Node program uninstalled successfully."
-            ;;
-        *)
-            echo "Uninstallation operation canceled."
-            ;;
-    esac
-}
-
-# Create wallet
-function add_wallet() {
-    artelad keys add wallet
-}
-
-# Import wallet
-function import_wallet() {
-    artelad keys add wallet --recover
-}
-
-# Check balances
-function check_balances() {
-    read -p "Please enter the wallet address: " wallet_address
-    artelad query bank balances "$wallet_address"
-}
-
-# Check node sync status
-function check_sync_status() {
-    artelad status | jq .SyncInfo
-}
-
-
-# Function to check service status (dummy function for demonstration)
-function check_service_status() {
-    pm2 list
-}
-
-# Function to view logs (dummy function for demonstration)
-function view_logs() {
-    pm2 logs artelad
-}
-
-# Function to uninstall node (dummy function for demonstration)
 function uninstall_node() {
     echo "Are you sure you want to uninstall the Artela node program? This will delete all related data. [Y/N]"
     read -r -p "Please confirm: " response
