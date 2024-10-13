cba0b451989b2f5245e3d068bff8a17e19d6d7da
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-10-13 15:13:55 +0700

    Update Unichain.sh

--- Unichain-Contract-Deployment/Unichain.sh
+++ Unichain-Contract-Deployment/Unichain.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Showing Animation.."
+wget -O loader.sh https://raw.githubusercontent.com/rmndkyl/MandaNode/main/WM/loader.sh && chmod +x loader.sh && sed -i 's/\r$//' loader.sh && ./loader.sh
+wget -O logo.sh https://raw.githubusercontent.com/rmndkyl/MandaNode/main/WM/logo.sh && chmod +x logo.sh && sed -i 's/\r$//' logo.sh && ./logo.sh
+sleep 4
+
 # Define text formats
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
