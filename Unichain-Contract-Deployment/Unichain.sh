4489b1cfc36ce3ac3da7582caad941c92c7b7faf
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-10-13 15:15:44 +0700

    Update Unichain.sh

--- Unichain-Contract-Deployment/Unichain.sh
+++ Unichain-Contract-Deployment/Unichain.sh
@@ -142,10 +142,10 @@ EOF
 main_menu() {
     while true; do
         clear
-        echo -e "{MENU_COLOR}Script and tutorial written by Telegram user @rmndkyl, free and open source, do not believe in paid versions${NORMAL}"
+        echo -e "${MENU_COLOR}Script and tutorial written by Telegram user @rmndkyl, free and open source, do not believe in paid versions${NORMAL}"
 	echo -e "${MENU_COLOR}${BOLD}============================ Unichain Contract Deployment ====================================${NORMAL}"
-	echo -e "{MENU_COLOR}Node community Telegram channel: https://t.me/layerairdrop${NORMAL}p"
-	echo -e "{MENU_COLOR}Node community Telegram group: https://t.me/+UgQeEnnWrodiNTI1${NORMAL}"
+	echo -e "${MENU_COLOR}Node community Telegram channel: https://t.me/layerairdrop${NORMAL}p"
+	echo -e "${MENU_COLOR}Node community Telegram group: https://t.me/+UgQeEnnWrodiNTI1${NORMAL}"
         echo -e "${MENU_COLOR}1. Deploy ERC-20 Token${NORMAL}"
         echo -e "${MENU_COLOR}2. Exit${NORMAL}"
         read -p "Enter an option (1-2): " OPTION
