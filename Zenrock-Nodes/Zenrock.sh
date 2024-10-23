77ddfd16afe4dcb9e695e0bb92016796418438c8
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-10-23 21:39:32 +0700

    Update Zenrock.sh

--- Zenrock-Nodes/Zenrock.sh
+++ Zenrock-Nodes/Zenrock.sh
@@ -47,7 +47,7 @@ function deploy_script() {
 
     # Download the binary
     mkdir -p $HOME/.zrchain/cosmovisor/genesis/bin
-    wget -O $HOME/.zrchain/cosmovisor/genesis/bin/zenrockd https://releases.gardia.zenrocklabs.io/zenrockd-4.7.1
+    wget -O $HOME/.zrchain/cosmovisor/genesis/bin/zenrockd https://releases.gardia.zenrocklabs.io/zenrockd-4.9.3
     chmod +x $HOME/.zrchain/cosmovisor/genesis/bin/zenrockd
 
     # Create symbolic links
