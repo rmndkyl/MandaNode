d87859a9828c22adf3566ef7702ba12c875f4db5
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-11-13 03:25:30 +0700

    Update Vana-SixGPT.sh

--- Vana-Nodes/Vana-SixGPT.sh
+++ Vana-Nodes/Vana-SixGPT.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Color codes for enhanced readability
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No color
+
 # Showing Logo
-echo "Showing Animation.."
+echo -e "${GREEN}Showing Animation...${NC}"
 wget -O loader.sh https://raw.githubusercontent.com/rmndkyl/MandaNode/main/WM/loader.sh && chmod +x loader.sh && sed -i 's/\r$//' loader.sh && ./loader.sh
+rm -rf loader.sh
 wget -O logo.sh https://raw.githubusercontent.com/rmndkyl/MandaNode/main/WM/logo.sh && chmod +x logo.sh && sed -i 's/\r$//' logo.sh && ./logo.sh
+rm -rf logo.sh
 sleep 4
 
 # Script save path
@@ -11,7 +20,7 @@ SCRIPT_PATH="$HOME/Vana-SixGPT.sh"
 
 # Check if the script is run as the root user
 if [ "$(id -u)" != "0" ]; then
-    echo "This script requires root user permissions to run."
+    echo -e "${RED}This script requires root user permissions to run.${NC}"
     echo "Please try switching to the root user using the 'sudo -i' command and run this script again."
     exit 1
 fi
@@ -20,17 +29,17 @@ fi
 function main_menu() {
     while true; do
         clear
-	echo "Script and tutorial written by Telegram user @rmndkyl, free and open source, do not believe in paid versions"
-	echo "============================ Vana SixGPT Installation ===================================="
-	echo "Node community Telegram channel: https://t.me/layerairdrop"
-	echo "Node community Telegram group: https://t.me/+UgQeEnnWrodiNTI1"
+        echo -e "${BLUE}============================ Vana SixGPT Installation ====================================${NC}"
+        echo -e "${YELLOW}Node community Telegram channel:${NC} https://t.me/layerairdrop"
+        echo -e "${YELLOW}Node community Telegram group:${NC} https://t.me/+UgQeEnnWrodiNTI1"
+        echo -e "${GREEN}Script and tutorial by Telegram user @rmndkyl, free and open source, do not believe in paid versions${NC}"
         echo "To exit the script, press ctrl + C."
-        echo "Please choose the operation you want to perform:"
-        echo "1) Start Node"
-        echo "2) View Logs"
-	echo "3) Restart Node"
-        echo "4) Delete Node"
-        echo "5) Exit"
+        echo -e "${GREEN}Please choose the operation you want to perform:${NC}"
+        echo -e "1) Start Node"
+        echo -e "2) View Logs"
+        echo -e "3) Restart Node"
+        echo -e "4) Delete Node"
+        echo -e "5) Exit"
         
         read -p "Please enter the number of your choice: " choice
         
@@ -41,18 +50,18 @@ function main_menu() {
             2)
                 view_logs
                 ;;
-	    3) 
-     		restart_node
-       		;;
+            3) 
+                restart_node
+                ;;
             4)
                 delete_node
                 ;;
             5)
-                echo "Exiting the script."
+                echo -e "${GREEN}Exiting the script.${NC}"
                 exit 0
                 ;;
             *)
-                echo "Invalid choice, please try again."
+                echo -e "${RED}Invalid choice, please try again.${NC}"
                 read -p "Press any key to continue..."
                 ;;
         esac
@@ -62,9 +71,11 @@ function main_menu() {
 # Function to start the node
 function start_node() {
     # Update package list and upgrade installed packages
+    echo -e "${YELLOW}Updating packages...${NC}"
     sudo apt update -y && sudo apt upgrade -y
 
     # Install required dependencies
+    echo -e "${YELLOW}Installing required dependencies...${NC}"
     sudo apt install -y ca-certificates zlib1g-dev libncurses5-dev libgdbm-dev \
     libnss3-dev tmux iptables curl nvme-cli git wget make jq libleveldb-dev \
     build-essential pkg-config ncdu tar clang bsdmainutils lsb-release \
@@ -72,7 +83,7 @@ function start_node() {
 
     # Check if Docker is installed
     if ! command -v docker &> /dev/null; then
-        echo "Docker is not installed, installing Docker..."
+        echo -e "${YELLOW}Docker is not installed, installing Docker...${NC}"
         
         # Install Docker
         sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
@@ -85,23 +96,23 @@ function start_node() {
         sudo systemctl start docker
         sudo systemctl enable docker
 
-        echo "Docker installation complete!"
+        echo -e "${GREEN}Docker installation complete!${NC}"
     else
-        echo "Docker is already installed, skipping installation."
+        echo -e "${GREEN}Docker is already installed, skipping installation.${NC}"
     fi
 
     # Check if Docker Compose is installed
     if ! command -v docker-compose &> /dev/null; then
-        echo "Docker Compose is not installed, installing Docker Compose..."
+        echo -e "${YELLOW}Docker Compose is not installed, installing Docker Compose...${NC}"
         
         # Get the latest version number and install Docker Compose
         VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
         curl -L "https://github.com/docker/compose/releases/download/$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         chmod +x /usr/local/bin/docker-compose
 
-        echo "Docker Compose installation complete!"
+        echo -e "${GREEN}Docker Compose installation complete!${NC}"
     else
-        echo "Docker Compose is already installed, skipping installation."
+        echo -e "${GREEN}Docker Compose is already installed, skipping installation.${NC}"
     fi
 
     # Display Docker Compose version
@@ -109,11 +120,11 @@ function start_node() {
 
     # Add the current user to the Docker group
     if ! getent group docker > /dev/null; then
-        echo "Creating Docker group..."
+        echo -e "${YELLOW}Creating Docker group...${NC}"
         sudo groupadd docker
     fi
 
-    echo "Adding user $USER to the Docker group..."
+    echo -e "${YELLOW}Adding user $USER to the Docker group...${NC}"
     sudo usermod -aG docker $USER
 
     # Create directory and set environment variables
@@ -125,9 +136,9 @@ function start_node() {
     export VANA_PRIVATE_KEY=$PRIVATE_KEY
 
     # Select network
-    echo "Please select a network (enter number 1 or 2):"
-    echo "1) satori(DONT USE, UNAVAILABLE!)"
-    echo "2) moksha(CHOOSE THIS!)" 
+    echo -e "${BLUE}Please select a network:${NC}"
+    echo -e "${YELLOW}1) satori (UNAVAILABLE!)"
+    echo -e "2) moksha (CHOOSE THIS!)${NC}"
     read -p "Please enter the number of your choice: " NETWORK_CHOICE
 
     case $NETWORK_CHOICE in
@@ -138,12 +149,12 @@ function start_node() {
             export VANA_NETWORK="moksha"
             ;;
         *)
-            echo "Invalid choice, defaulting to moksha."
+            echo -e "${RED}Invalid choice, defaulting to moksha.${NC}"
             export VANA_NETWORK="moksha"
             ;;
     esac
 
-    echo "Selected network: $VANA_NETWORK"
+    echo -e "${GREEN}Selected network: $VANA_NETWORK${NC}"
 
     # Create docker-compose.yml file
     cat <<EOL > docker-compose.yml
@@ -175,41 +186,41 @@ volumes:
 EOL
 
     # Start Docker Compose
-    echo "Starting Docker Compose..."
+    echo -e "${GREEN}Starting Docker Compose...${NC}"
     docker-compose up -d
-    echo "Docker Compose has started!"
-    echo "All operations completed! Please log out and back in to apply group changes."
+    echo -e "${GREEN}Docker Compose has started!${NC}"
+    echo -e "${GREEN}All operations completed! Please log out and back in to apply group changes.${NC}"
 
     read -p "Press any key to return to the main menu..."
 }
 
 # Function to view logs
 function view_logs() {
-    echo "Viewing Docker Compose logs..."
+    echo -e "${YELLOW}Viewing Docker Compose logs...${NC}"
     cd $HOME/sixgpt && docker-compose logs -f
     read -p "Press any key to return to the main menu..."
 }
 
 # Function to restart the node
 function restart_node() {
-    echo "Entering /root/sixgpt directory..."
-    cd /root/sixgpt || { echo "Directory does not exist!"; return; }
+    echo -e "${YELLOW}Entering /root/sixgpt directory...${NC}"
+    cd /root/sixgpt || { echo -e "${RED}Directory does not exist!${NC}"; return; }
 
-    echo "Restarting all Docker Compose services..."
+    echo -e "${GREEN}Restarting all Docker Compose services...${NC}"
     docker-compose restart
-    echo "All Docker Compose services have been restarted!"
+    echo -e "${GREEN}All Docker Compose services have been restarted!${NC}"
     
     read -p "Press any key to return to the main menu..."
 }
 
 # Function to delete the node
 function delete_node() {
-    echo "Entering /root/sixgpt directory..."
-    cd /root/sixgpt || { echo "Directory does not exist!"; return; }
+    echo -e "${YELLOW}Entering /root/sixgpt directory...${NC}"
+    cd /root/sixgpt || { echo -e "${RED}Directory does not exist!${NC}"; return; }
 
-    echo "Stopping all Docker Compose services..."
+    echo -e "${RED}Stopping all Docker Compose services...${NC}"
     docker-compose down
-    echo "All Docker Compose services have been stopped!"
+    echo -e "${GREEN}All Docker Compose services have been stopped!${NC}"
     
     read -p "Press any key to return to the main menu..."
 }
